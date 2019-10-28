e4f7c67480076a402b88343253ba853918ba0e47
Neville Cox <neville.cox@outlook.com>
2019-10-28 17:27:58 +0000

    Add project files.

---
+++ BlazorApp_JsonPropertyIssue/Shared/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BlazorApp_JsonPropertyIssue.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        [JsonProperty("sum")]
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
